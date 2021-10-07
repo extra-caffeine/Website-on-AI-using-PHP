e33eeb8bd960ac8923f643d8ce05bc40a5adf171
Keshav <50097887+extra-caffeine@users.noreply.github.com>
2021-10-07 22:12:28 +0530

    Add files via upload

---
+++ foot/about.php
@@ -0,0 +1,2 @@
+Computer will overtake humans with A! within next 100 years. When that happens, we need to make sure the computer have goals aligned with ours.<br>
+<div class="name"><b>--STEPHEN HAWKING</div></b>
